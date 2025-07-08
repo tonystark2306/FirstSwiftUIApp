1c1c199216e9001be2f064833362351a973bf75b
tonystark2306 <dat.ntt210177@sis.hust.edu.vn>
2025-07-08 08:58:40 +0700

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by iKame Elite Fresher 2025 on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstSwiftUIAppTests/FirstSwiftUIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppTests.swift
+//  FirstSwiftUIAppTests
+//
+//  Created by iKame Elite Fresher 2025 on 7/8/25.
+//
+
+import Testing
+@testable import FirstSwiftUIApp
+
+struct FirstSwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
